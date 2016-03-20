68652536f583dd358f42a2584eea898a595dc928
jerome <jerometea@gmail.com>
2016-03-20 12:07:04 +0100

    bdd test

---
+++ src/logical/Test.java
@@ -0,0 +1,45 @@
+package logical;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javafx.util.converter.LocalDateStringConverter;
+
+public class Test {
+
+	public static void main(String[] args) throws SQLException {
+		BaseDeDonnees bdd = new BaseDeDonnees();
+		
+		try {
+			bdd.getConnection("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/projetjava", "root", "");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		Personne p = new Personne(1, "Morel", "Christophe", "Paris",LocalDate.of(1989, 6, 9), 75, "0182487585");
+		bdd.insertPersonne(p);
+		
+		Personne p1 = bdd.getPersone(p);
+		System.out.println(p1.getNom());
+		
+		ArrayList<Personne> listPersonne = new ArrayList<Personne>();
+		listPersonne = bdd.GetEverybody();
+		for(Personne personne : listPersonne){
+			System.out.println(personne.getNom());
+		}
+		
+		p1.setNom("Mozarel");
+		p1.setPrenom("pablo");
+		p1.setNaissance(LocalDate.of(1990,10,2));
+		bdd.UpdatePersonne(p1);
+		p = bdd.getPersone(p1);
+		System.out.println(p1.getNom() + " " + p1.getPrenom() + " " + p1.getNaissance() + " ");
+		
+		
+		bdd.CloseConnection();
+
+	}
+
+}
